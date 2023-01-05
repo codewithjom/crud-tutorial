9904af803940049481143a8184a30d90f9223b05
Jom Dollesin <codewithjom@gmail.com>
2023-01-05 22:13:30 +0800

    feat: added prettier.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,49 +1,49 @@
-import type { NextPage } from "next";
-import { useState } from "react";
+import type { NextPage } from 'next'
+import { useState } from 'react'
 
 interface FormData {
-  title: string;
-  content: string;
-  id: string;
+  title: string
+  content: string
+  id: string
 }
 
 const Home: NextPage = () => {
   const [form, setForm] = useState<FormData>({
-    title: "",
-    content: "",
-    id: "",
-  });
+    title: '',
+    content: '',
+    id: ''
+  })
 
   async function create(data: FormData) {
     try {
-      fetch("http://localhost:3000/api/create", {
+      fetch('http://localhost:3000/api/create', {
         body: JSON.stringify(data),
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json'
         },
-        method: "POST",
-      }).then(() => setForm({ title: "", content: "", id: "" }));
+        method: 'POST'
+      }).then(() => setForm({ title: '', content: '', id: '' }))
     } catch (error) {
-      console.log(error);
+      console.log(error)
     }
   }
 
   const handleSubmit = async (data: FormData) => {
     try {
-      create(data);
+      create(data)
     } catch (error) {
-      console.log(error);
+      console.log(error)
     }
-  };
+  }
 
   return (
     <>
       <div>
         <h1 className="mt-8 text-3xl font-bold text-center">Notes</h1>
         <form
-          onSubmit={(e) => {
-            e.preventDefault();
-            handleSubmit(form);
+          onSubmit={e => {
+            e.preventDefault()
+            handleSubmit(form)
           }}
           className="w-auto min-w-[25%] max-w-min mx-auto space-y-6 flex flex-col items-stretch"
         >
@@ -51,13 +51,13 @@ const Home: NextPage = () => {
             type="text"
             placeholder="Title"
             value={form.title}
-            onChange={(e) => setForm({ ...form, title: e.target.value })}
+            onChange={e => setForm({ ...form, title: e.target.value })}
             className="p-1 border-2 border-gray-600 rounded"
           />
           <textarea
             placeholder="Content"
             value={form.content}
-            onChange={(e) => setForm({ ...form, content: e.target.value })}
+            onChange={e => setForm({ ...form, content: e.target.value })}
             className="p-1 border-2 border-gray-600 rounded"
           />
           <button type="submit" className="p-1 text-white bg-blue-500 rounded">
@@ -66,7 +66,7 @@ const Home: NextPage = () => {
         </form>
       </div>
     </>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
